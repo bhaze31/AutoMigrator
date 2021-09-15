74262a9e0d51d36329c8879e869423664be8ff49
Brian Hasenstab <brian.hasenstab31@gmail.com>
2021-09-15 17:25:36 -0400

    Make methods/variables/class of AutoMigration open instead of public

--- Sources/AutoMigrator/AutoMigration.swift
+++ Sources/AutoMigrator/AutoMigration.swift
@@ -8,22 +8,22 @@
 import Fluent
 
 /// AutoMigration is a class that allows us to dynamically load all migrations in a certain directory. It does this by conforming to the Migration protocol, allowing us to add it to app.migrations. It then overrides certain properties that Migration uses to add the migration to the database, forcing us in our subclasses to actually define them.
-public class AutoMigration: Migration {
-    public var name: String {
+open class AutoMigration: Migration {
+    open var name: String {
         fatalError("Must define name of AutoMigration in subclass")
     }
     
-    public var defaultName: String {
+    open var defaultName: String {
         fatalError("Must define defaultName of AutoMigration in subclass")
     }
     
-    public func prepare(on database: Database) -> EventLoopFuture<Void> {
+    open func prepare(on database: Database) -> EventLoopFuture<Void> {
         fatalError("Subclass must override prepare")
     }
     
-    public func revert(on database: Database) -> EventLoopFuture<Void> {
+    open func revert(on database: Database) -> EventLoopFuture<Void> {
         fatalError("Subclass must override revert")
     }
     
-    required init() {}
+    required public init() {}
 }
