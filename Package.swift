eb32f1b2cf994754c0c94afc5e355bdaf745c82f
Brian Hasenstab <brian.hasenstab31@gmail.com>
2021-11-10 14:57:48 -0500

    Moving to new Mac

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.30.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.48.8"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.3.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
